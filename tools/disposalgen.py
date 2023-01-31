c9dfc1ffcc7c5adbefb409d31bd43f5d33dc4933
Herma <49307226+hermaplusplus@users.noreply.github.com>
2023-01-31 17:33:25 +0000

    Disposalgen tool comment collapse

--- tools/disposalgen.py
+++ tools/disposalgen.py
@@ -1,17 +1,8 @@
-# Generates disposals code from a list of variable declarations.
-# Not all object can be disposed, so remove any flagged by your IDE.
+# Rudimentarily generates disposals code from a list of variable declarations (not all variables are actually disposable).
 # Example usage:
 #   python .\disposalgen.py """OrthographicCamera camera;
 #   >>     SpriteBatch batch;
-#   >>     Animation<TextureRegion> introAnimation;
-#   >>     Texture introSheet;
-#   >>     BitmapFont font;
-#   >>     float stateTime;
-#   >>     int FRAME_COLS;
-#   >>     int FRAME_ROWS;
-#   >>     int winWidth;
-#   >>     int winHeight;"""
+#   >>     BitmapFont font;"""
 
 import sys
-
 print("\n".join([k.replace(";", ".dispose();") for k in [j.split(" ")[1] for j in [i.strip("\t ") for i in sys.argv[1:][0].split("\n")]]]))
